f1ddb578d5bc7692d43f2408bb64d09b750dcc20
Roman Lysenko <romashkaxxxl@gmail.com>
2022-09-10 18:22:10 +0300

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,15 +1,14 @@
-async function fetchCountries(country) {
+function fetchCountries(country) {
   const BASE_URL = 'https://restcountries.com/v2/name/';
   const filtersCountries = '?fields=flag,capital,name,population,languages';
-  try {
-    const data = await fetch(`${BASE_URL}${country}${filtersCountries}`);
-    if (!data.ok) {
-      throw new Error(
-        Notify.failure('Oops, there is no country with that name')
-      );
-    }
-    return await data.json();
-  } catch (err) {
-    return console.error(err);
-  }
+  return fetch(`${BASE_URL}${country}${filtersCountries}`)
+    .then(data => {
+      if (!data.ok) {
+        throw new Error(
+          Notify.failure('Oops, there is no country with that name')
+        );
+      }
+      return data.json();
+    })
+    .catch(err => console.error(err));
 }
